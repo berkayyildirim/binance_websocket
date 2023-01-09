b20778343f6202867a1c89773a4e183d0d282f38
berkayyildirim <huseyinberkayyildirim@gmail.com>
2023-01-09 15:53:37 +0300

    remove ejs package and refactor index file

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-// let stockPriceElement = document.getElementById("stock-price");
-// let ws = new WebSocket("wss://stream.binance.com:9443/ws/btcusdt@trade");
-// ws.onmessage = (event) => {
-//   let stockObject = JSON.parse(event.data);
-//   stockPriceElement.innerText = parseFloat(stockObject.p).toFixed(2);
-//   console.log(stockObject.p);
-// };
-
 // Package requirements
 const WebSocket = require("ws");
 
