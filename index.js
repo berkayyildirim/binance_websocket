295fbbbf1b4f486daa6019bd67e4b401c0ffa95f
berkayyildirim <huseyinberkayyildirim@gmail.com>
2023-01-10 15:23:47 +0300

    implement websocket stream rendering from server to client

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
-// Package requirements
+// Binance WebSocket Stream Package requirements
 const WebSocket = require("ws");
+
+// WebSocket Server Requirement
+const { Server } = require("ws");
 
 // Node.js server requirements
 const express = require("express");
 const app = express();
 
-// Binance stream
+// Create Browser's Server
+const server = express()
+  .use((req, res) => res.sendFile("/index.html", { root: __dirname }))
+  .listen(3000, () => console.log(`Listening on ${3000}`));
+
+// Get Binance Stream with WebSocket
 const ws = new WebSocket(
   "wss://stream.binance.com/stream?streams=btcusdt@ticker_1h"
 );
 
-// Define a new array to add stream data
-let tickerData = [];
+// Create WebSocket Server
+const webSocketServer = new Server({ port: 8080 });
 
-// Get data from stream with WebSocket
-ws.on("message", function message(data) {
-  let parsedData = JSON.parse(data);
-  // Add data as json object to use GET API
-  tickerData.push(parsedData);
-});
-
-// GET API
-app.route("/").get(function (req, res, next) {
-  res.json(tickerData);
-});
-
-// Define server port as localhost:8080
-app.listen(8080, () => {
-  console.log(`Server listening on http://localhost:${8080}`);
-});
+// Send Data to the Client
+setInterval(() => {
+  webSocketServer.clients.forEach((client) => {
+    // Send Data to the Client
+    ws.on("message", function message(data) {
+      let parsedData = JSON.parse(data);
+      // Send the last price
+      client.send(parsedData.data.c);
+    });
+  });
+}, 1000);
